a589f88d572242b4809275e673671eb369d878ef
Cheuk Li <cheuk.li@bamboonetworks.com>
2017-03-29 10:45:59 +0800

    scss

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-// require ("./assets/css/ui.scss")
 // import './assets/css/ui.scss'
 
 Vue.config.productionTip = false
